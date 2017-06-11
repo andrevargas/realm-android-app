50ca6925907f4bf2b392b59f8d59e76f81502df5
GustavoDalPont <gustavodpont@gmail.com>
2017-06-11 19:44:32 -0300

    Saving board to realm, It's working, trust me, I'm an engineer :suspect:

--- app/src/main/java/br/univali/sisnet/realmapp/fragments/BoardListFragment.kt
+++ app/src/main/java/br/univali/sisnet/realmapp/fragments/BoardListFragment.kt
@@ -10,11 +10,14 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import br.univali.sisnet.realmapp.R
 import br.univali.sisnet.realmapp.domain.Board
 import br.univali.sisnet.realmapp.domain.Todo
 import br.univali.sisnet.realmapp.view.adapters.BoardAdapter
+import io.realm.Realm
 import io.realm.RealmList
+import io.realm.RealmResults
 
 class BoardListFragment : Fragment() {
 
@@ -22,6 +25,7 @@ class BoardListFragment : Fragment() {
     var fabAddBoard: FloatingActionButton? = null
     var adapter: BoardAdapter? = null
     var listener: OnItemSelectedListener? = null
+    var realm: Realm? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
           savedInstanceState: Bundle?): View? {
@@ -33,6 +37,8 @@ class BoardListFragment : Fragment() {
         rvBoards = view!!.findViewById(R.id.rvBoards) as RecyclerView?
         fabAddBoard = view.findViewById(R.id.fabAddBoard) as FloatingActionButton?
         fabAddBoard!!.setOnClickListener(this::onClickAddBoard)
+        Realm.init(view.context)
+        realm = Realm.getDefaultInstance()
         setupRecyclerView()
     }
 
@@ -42,10 +48,7 @@ class BoardListFragment : Fragment() {
         val layoutManager = LinearLayoutManager(context)
 
         adapter = BoardAdapter(listener)
-        adapter!!.boardList = listOf(
-            Board(1, "Quadrinho", RealmList<Todo>()),
-            Board(2, "Outro quadrinho", RealmList<Todo>())
-        )
+        adapter!!.boardList = realm?.where(Board::class.java)!!.findAll()
 
         rvBoards!!.layoutManager = layoutManager
         rvBoards!!.adapter = adapter
@@ -63,6 +66,20 @@ class BoardListFragment : Fragment() {
 
     private fun saveBoard(view: View) {
 
+        val etBoardName = view.findViewById(R.id.etBoardName) as EditText
+        val board: Board = Board()
+
+        try {
+            board.id = realm?.where(Board::class.java)!!.max("id").toLong() + 1
+        } catch (ex: ArrayIndexOutOfBoundsException) {
+            board.id = 0
+        }
+
+        board.name = etBoardName.text.toString()
+
+        realm?.beginTransaction()
+        realm?.copyToRealm(board)
+        realm?.commitTransaction()
     }
 
     override fun onAttach(context: Context?) {
